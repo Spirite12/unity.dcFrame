34dcba144b2dc6b171dad73c9f50f5e68b52ac84
斯比尔特 <spirit12@qq.com>
2025-01-22 17:26:55 +0800

    <fix> UIBase 和 UImgr 转换成异步操作

--- Modules/UIManager/UIMgr.cs
+++ Modules/UIManager/UIMgr.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace DCFrame {
-	/// <summary>
-	/// UI管理类，自动管理层级
-	/// 术语定义：
-	/// 1.显示关闭界面，在游戏逻辑中主动调用UIBase.Close方法；
-	/// 2.隐式关闭界面，在UIMgr中因为层级关系被关闭。
-	/// </summary>
 	public class UIMgr : Singleton<UIMgr> {
-
-        /// <summary>
-        /// 关闭所有UI的回调，方便做资源回收
-        /// </summary>
-        public event Action OnCloseAllUI;
-        /// <summary>
-        /// 打开关闭游戏弹窗的通知
-        /// </summary>
-        public event Action OnShowClosePanel;
 		/// <summary>
 		/// UI根结点
 		/// </summary>
@@ -35,9 +20,6 @@ namespace DCFrame {
 		/// <summary>
 		/// 初始化UIMgr。
 		/// </summary>
-		/// <param name="root">UI根节点</param>
-		/// <param name="deActiveRoot"></param>
-		/// <param name="camera"></param>
 		public void Init(Transform uiRoot, Transform deActiveRoot, Camera uiCamera) {
 			this.uiRoot = uiRoot;
 			this.deActiveRoot = deActiveRoot;
@@ -55,26 +37,14 @@ namespace DCFrame {
         /// Escape关闭窗口
         /// </summary>
         public void EscapeClose() {
-            // 关闭游戏确认弹窗
-            void openClosePanel() {
-                OnShowClosePanel?.Invoke();
-            }
-
             for (int i = uiStack.Count - 1; i > 0; i--) {
                 if (uiStack[i].IsOpen()) {
                     uiStack[i].Close();
-                    return;
+                    break;
                 }
             }
-            openClosePanel();
         }
 
-        /// <summary>
-		/// UIBase打开的时候通过这个函数通知UIMgr进行层级管理，
-		/// 如果已经在UI栈里则返回false，不能重复打开同一个界面。
-		/// </summary>
-		/// <param name="ui">被打开的界面</param>
-		/// <returns>是否打开成功</returns>
 		public bool Open(UIBase ui) {
 			if (uiStack.IndexOf(ui) >= 0) {
 				return false;
@@ -100,25 +70,14 @@ namespace DCFrame {
 			return true;
 		}
 
-		/// <summary>
-		/// UIBase显式关闭的时候通过这个函数通知UIMgr进行管理。
-		/// </summary>
-		/// <param name="ui">被关闭的界面</param>
-		/// <param name="callback">关闭回调，因为关闭当前界面有可能需要异步返回之前被隐式关闭的界面，所以关闭也是异步的</param>
-		public void Close(UIBase ui, Action<Exception> callback) {
-            void doClose(Exception ex) {
-                int idx = uiStack.IndexOf(ui);
-                uiStack.Remove(ui);
-                if (idx < uiStack.Count) {
-                    SortAboveUI(uiStack[idx]);
-                }
-                callback(ex);
-			}
-
+		public async UniTask Close(UIBase ui) {
 			if (ui.GetUIBaseType() == UIBase.UIBaseType.FullScene) {
-				OpenBelowUI(ui, doClose);
-			} else {
-				doClose(null);
+				await OpenBelowUI(ui);
+			}
+			int idx = uiStack.IndexOf(ui);
+			uiStack.Remove(ui);
+			if (idx < uiStack.Count) {
+				SortAboveUI(uiStack[idx]);
 			}
 		}
 
@@ -216,10 +175,7 @@ namespace DCFrame {
 		/// <summary>
 		/// 关闭当前UI之后的UI
 		/// </summary>
-		/// <param name="ui"></param
-		/// <param name="callBack"></param>
-		/// <returns>是否有UI被关闭</returns>
-		public void CloseAboveUI(UIBase ui, Action<Exception> callBack) {
+		public async UniTask CloseAboveUI(UIBase ui) {
             for (int i = uiStack.Count - 1; i >= 0; i--) {
                 if (uiStack[i].Equals(ui)) {
                     if (uiStack[i].IsOpen()) {
@@ -228,13 +184,13 @@ namespace DCFrame {
 					break;
                 }
                 if (uiStack[i].IsOpen() && uiStack[i].IsDefaultUIBaseType()) {
-                    uiStack[i].Close(null, false);
+                    uiStack[i].Close(false);
                 }
                 if (uiStack[i].IsDefaultUIBaseType()) {
                     uiStack.RemoveAt(i);
 				}
 			}
-			OpenBelowUI(ui, callBack);
+			await OpenBelowUI(ui);
         }
 
 		#region Private Methods
@@ -259,94 +215,66 @@ namespace DCFrame {
         }
 
         /// <summary>
-        /// 打开指定UI下的UI，直到打开一个全屏不透明界面
+        /// 打开当前UI之前的界面，直到打开一个全屏界面
         /// </summary>
-        /// <param name="ui"></param>
-        /// <param name="callback"></param>
-        private void OpenBelowUI(UIBase ui, Action<Exception> callback) {
+        private async UniTask OpenBelowUI(UIBase ui) {
 			var idx = uiStack.IndexOf(ui);
 			if (idx <= 0) {
-				callback?.Invoke(null);
 				return;
 			}
 
-			var expectedOpenCnt = 0;
-			var actualOpenedCnt = 0;
-			var loopFinished = false;
-			Exception lastException = null;
-
-			void NextCallback() {
-				if (expectedOpenCnt != actualOpenedCnt || !loopFinished) {
-					return;
-				}
-				callback?.Invoke(lastException);
-				callback = null;// 置空，防止重复调用
-			}
-
+			List<UniTask> taskList = new List<UniTask>();
 			for(var i = idx - 1; i >= 0; --i) {
                 if (!uiStack[i].IsDefaultUIBaseType()) {
 					continue;
                 }
-                if(uiStack[i].IsOpen() && uiStack[i].GetUIBaseType() == UIBase.UIBaseType.FullScene) {
-                    loopFinished = true;
-                    NextCallback();
-					return;
+                bool isFullScene = uiStack[i].GetUIBaseType() == UIBase.UIBaseType.FullScene;
+                if(uiStack[i].IsOpen() && isFullScene) {
+                    break;
 				}
-				++expectedOpenCnt;
-                // 这个open如果界面常驻内存就有可能会变成同步操作，使用loopFinished防止回调被提前调用
-                uiStack[i].Open(exception => {
-					if(exception != null) {
-						lastException = exception;
-					}
-					++actualOpenedCnt;
-					NextCallback();
-				},null,true);
-				if(uiStack[i].GetUIBaseType() == UIBase.UIBaseType.FullScene) {
+				var task = uiStack[i].Open(null,true);
+				taskList.Add(task);
+				if(isFullScene) {
 					break;
 				}
 			}
-			loopFinished = true;
-			NextCallback();
+			await UniTask.WhenAll(taskList);
 		}
 
 		/// <summary>
-		/// 关闭全屏之前以上的界面
+		/// 关闭当前界面之前的界面
 		/// </summary>
-		/// <param name="ui"></param>
 		private void CloseBelowUI(UIBase ui) {
 			var idx = uiStack.IndexOf(ui);
 			if (idx <= 0) {
 				return;
 			}
-
-			for (var i = idx - 1;i >= 0;--i) {
+			for (var i = idx - 1; i >= 0;--i) {
 				if (!uiStack[i].IsReturnable()) {
 					uiStack.RemoveAt(i);
 				}
 				if (!uiStack[i].IsOpen()) {
 					continue;
 				}
-                uiStack[i].Close(null, false, false);
+                uiStack[i].Close(false, false);
 			}
 		}
 
         /// <summary>
-        /// 关闭所有未关闭的界面，根据参数决定是否强制卸载所有未卸载的界面。
+        /// 关闭所有未关闭的界面
         /// </summary>
-        /// <param name="forceUnload"></param>
         private void CloseAllUI() {
             var tmpUIs = new List<UIBase>();
-            for (var i = 0; i < uiStack.Count; ++i) {
-                tmpUIs.Add(uiStack[i]);
+            foreach (var t in uiStack) {
+	            tmpUIs.Add(t);
             }
-
             // UIBase.Close接口会删除uiList数组元素，所以引用临时数据
             for (var i = tmpUIs.Count - 1; i >= 0; --i) {
-                tmpUIs[i].Close(null, false);
+	            if (tmpUIs[i].IsOpen()) {
+		            tmpUIs[i].Close(false);
+	            }
             }
-
             uiStack.Clear();
-            OnCloseAllUI?.Invoke();
         }
 
     #endregion
