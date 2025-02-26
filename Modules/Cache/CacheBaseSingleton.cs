68557ad31b43e60092ae8f0317e8337d313c8d75
斯比尔特 <spirit12@qq.com>
2025-02-26 17:48:25 +0800

    <feature> 新增本地缓存的相关代码

---
+++ Modules/Cache/CacheBaseSingleton.cs
@@ -0,0 +1,10 @@
+﻿namespace DCFrame {
+    public abstract class CacheBaseSingleton<T> : CacheBase where T : class, new() {
+
+        protected CacheBaseSingleton() { }
+
+        public static T Instance { get; } = new T();
+    }
+}
+
+
